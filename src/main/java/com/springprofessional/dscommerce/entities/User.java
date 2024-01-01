ffda8847573cc1de7d18c64aaa40c2345067c559
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-01-01 11:10:27 -0300

    Criado Order

--- src/main/java/com/springprofessional/dscommerce/entities/User.java
+++ src/main/java/com/springprofessional/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.springprofessional.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,8 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
     public User(){
     }
 
@@ -76,5 +80,7 @@ public class User {
         this.password = password;
     }
 
-
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
