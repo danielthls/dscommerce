a59bef33ce901fbbe5b0c1e28826aa948e907af6
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-01-19 20:07:05 -0300

    Product Insert

--- src/main/java/com/springprofessional/dscommerce/services/ProductService.java
+++ src/main/java/com/springprofessional/dscommerce/services/ProductService.java
@@ -24,4 +24,16 @@ public class ProductService {
     public Page<ProductDTO> findAll(Pageable pageable) {
         return repository.findAll(pageable).map(x -> new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
 }
